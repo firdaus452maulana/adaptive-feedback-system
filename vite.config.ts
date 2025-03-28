a9046b41319fe00dd3fdc3dd7921f0dac841948a
firdaus452maulana <firdaus452maulana@gmail.com>
2025-03-28 23:24:02 +0900

    update port

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     cors: false,
-    port: 2303,
+    port: 2302,
     proxy: {
       '/api': {
           target: 'https://api-adaptive.firdausmaulana.com',
